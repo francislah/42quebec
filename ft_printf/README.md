15c1e14e28d5da4f780264e8aefd267ef3a15401
Francis Lahoud <flahoud@student.42quebec.com>
2023-11-04 15:21:47 -0400

    add ft_printf

---
+++ ft_printf/README.md
@@ -0,0 +1,2 @@
+# ft_printf
+My version of printf function for 42Quebec
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flahoud <flahoud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 16:50:14 by flahoud           #+#    #+#             */
+/*   Updated: 2022/04/13 14:12:41 by flahoud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include "libft/libft.h"
+
+int	ft_printf(const char *str, ...);
+
+#endif
